3d219f0e9e8b0852c8ecb0bea02f0512d6d20817
Zheng Wang <cerebronzw@gmail.com>
2023-02-01 18:59:16 -0500

    use hashroute

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: "", component: AppComponent },
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
