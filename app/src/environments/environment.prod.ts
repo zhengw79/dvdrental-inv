457287ce1803f405c73d42a20430cf113e5c91cb
Zheng Wang <cerebronzw@gmail.com>
2023-02-07 14:27:52 -0500

    update film components

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://0bef-2607-fea8-4a5c-5f59-fcc2-2eb9-23a6-d41c.ngrok.io"
+  apiUrl: "https://3fb3-2607-fea8-4a5c-5f59-108d-8d8c-b7f1-44f.ngrok.io"
 };
