de02f47a72d7fcbd1118b6dfef93d3fa70ce9e22
Zheng Wang <cerebronzw@gmail.com>
2023-02-13 14:58:54 -0500

    main update remote url link

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://9bab-2607-fea8-4a5c-5f59-283a-7a65-f7e6-8176.ngrok.io/"
+  apiUrl: "https://daf0-2607-fea8-4a5c-5f59-193-f7bd-288c-2b20.ngrok.io/"
 };
