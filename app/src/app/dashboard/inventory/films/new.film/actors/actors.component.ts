08d56cc1be65112311226fc7cdff9676f96bdbba
Zheng Wang <cerebronzw@gmail.com>
2023-02-17 10:32:34 -0500

    main build actor component

--- app/src/app/dashboard/inventory/films/new.film/actors/actors.component.ts
+++ app/src/app/dashboard/inventory/films/new.film/actors/actors.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-new-film-actors',
@@ -8,15 +8,34 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 })
 export class ActorsComponent implements OnInit {
   formModel: FormGroup = new FormGroup({
-    first_name: new FormArray([new FormControl()]),
-    // last_name: new FormArray([new FormControl()])
-  })
+    actors: new FormArray([new FormGroup({
+      first_name: new FormControl(),
+      last_name: new FormControl()
+    })])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getFirstNameControls() {
-    return (this.formModel.get("first_name") as FormArray).controls;
+  getActorControls() {
+    return (this.formModel.get("actors") as FormArray).controls
+  }
+
+  getActors() {
+    return this.formModel.get("actors") as FormArray;
+  }
+
+  addActor() {
+    (this.formModel.get("actors") as FormArray).push(new FormGroup({
+      first_name: new FormControl(),
+      last_name: new FormControl()
+    }));
+  }
+
+  removeActor(index: number) {
+    console.log(index);
+    (this.formModel.get("actors") as FormArray).removeAt(index);
   }
 }
