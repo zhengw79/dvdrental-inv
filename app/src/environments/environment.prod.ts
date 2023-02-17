e200972dff7e42cf82e5f2af01ba1d78d240d3be
Zheng Wang <cerebronzw@gmail.com>
2023-02-17 08:44:54 -0500

    main update portal url

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://5134-2607-fea8-4a5c-5f59-d4d8-991c-6737-832f.ngrok.io/"
+  apiUrl: "https://41e4-2607-fea8-4a5c-5f59-485e-a7ad-f740-ca13.ngrok.io/"
 };
