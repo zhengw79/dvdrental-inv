2e3b47a7c4ca0058cc14ac8f131754767dd1e4af
Zheng Wang <cerebronzw@gmail.com>
2023-02-18 22:39:23 -0500

    main update film info form

--- app/src/app/dashboard/inventory/films/new.film/info/info.component.ts
+++ app/src/app/dashboard/inventory/films/new.film/info/info.component.ts
@@ -12,13 +12,20 @@ export class InfoComponent implements OnInit {
 
   @ViewChild("title_el") title_el?: ElementRef<any>;
   @ViewChild("replacement_cost_el") replacement_cost_el?: ElementRef<any>;
+  @ViewChild("year_el") year_el?: ElementRef<any>;
   @ViewChild("rental_rate_el") rental_rate_el?: ElementRef<any>;
   fg_film: FormGroup;
+  @ViewChild("category_el") category_el?: ElementRef<any>;
+  @ViewChild("language_el") language_el?: ElementRef<any>;
+  @ViewChild("length_el") length_el?: ElementRef<any>;
 
-  years: Array<number> = [];
-  categories: Array<any> = [];
-  languages: Array<any> = [];
+  // @ts-ignore
+  $: any = window.jQuery;
   ratings: Array<string> = [];
+  years: Array<number> = [];
+  sel_year: any;
+  sel_categories: any;
+  sel_language: any;
 
   constructor(
     private filmService: FilmService
@@ -27,12 +34,13 @@ export class InfoComponent implements OnInit {
       title: new FormControl("", [Validators.required]),
       description: new FormControl("", [Validators.required]),
       rental_rate: new FormControl("", [Validators.required]),
-      rating: new FormControl("", [Validators.required]),
-      length: new FormControl("", [Validators.required]),
-      replacement_cost: new FormControl("", [Validators.required]),
+      rating: new FormControl(null, [Validators.required]),
+      length: new FormControl("", [Validators.required, Validators.max(250), Validators.min(0)]),
+      replacement_cost: new FormControl("", [Validators.required, Validators.max(10), Validators.min(1)]),
       rental_duration: new FormControl("", [Validators.required]),
       category: new FormControl("", [Validators.required]),
-      language: new FormControl("", [Validators.required])
+      language: new FormControl("", [Validators.required]),
+      year: new FormControl("", [Validators.required])
     });
 
     for (let i = 1901; i <= 2155; i++) {
@@ -48,38 +56,104 @@ export class InfoComponent implements OnInit {
     return this.fg_film.get("rental_rate");
   }
 
+  get rental_duration() {
+    return this.fg_film.get("rental_duration");
+  }
+
+  get year() {
+    return this.fg_film.get("year");
+  }
+
+  get category() {
+    return this.fg_film.get("category");
+  }
+
+  get language() {
+    return this.fg_film.get("language");
+  }
+
+  get rating() {
+    return this.fg_film.get("rating");
+  }
+
+  get length() {
+    return this.fg_film.get("length");
+  }
+
+  get replacement_cost() {
+    return this.fg_film.get("replacement_cost");
+  }
+
   async ngOnInit(): Promise<void> {
     const { fetchFilmSettings: { categories, languages, ratings } } = await this.filmService.fetchFilmSettings();
+
     this.ratings = ratings;
-    this.languages = languages.map((language: any) => ({ language_id: language.language_id, name: language.name }));
-    this.categories = categories.map((category: any) => ({ category_id: category.category_id, name: category.name }));
+    this.sel_language = this.$(this.language_el?.nativeElement).select2({
+      "data": languages.map((language: any) => ({
+        "id": language.language_id,
+        "text": language.name
+      }))
+    }).on("select2:select", (event: any) => {
+      const { id } = event.params.data;
+      console.log(id);
+      this.language?.setValue(+id);
+    });
+
+    this.sel_categories = this.$(this.category_el?.nativeElement).select2({
+      "placeholder": "Select a film's category.",
+      "allowClear": true,
+      "data": categories.map((category: any) => ({
+        "id": category.category_id,
+        "text": category.name
+      })),
+      "maximumSelectionLength": 2
+    }).on("select2:select", (event: any) => {
+      const cats = this.sel_categories.val().join(",");
+      this.category?.setValue(cats);
+    });
   }
 
   ngAfterViewInit() {
-    // IMask(this.title_el?.nativeElement, {
-    //   mask: '+{7}(000)000-00-00',
-    //   lazy: false,  // make placeholder always visible
-    //   placeholderChar: '_'     // defaults to '_'
+    // IMask(this.rental_rate_el?.nativeElement, {
+    //   mask: Number,
+    //   scale: 2,
+    //   radix: ".",
+    //   signed: false,
+    //   max: 100,
+    //   lazy: false
     // });
-    IMask(this.rental_rate_el?.nativeElement, {
-      mask: Number,
-      scale: 2,
-      radix: ".",
-      signed: false,
-      max: 100,
-      lazy: false
-    });
-    IMask(this.replacement_cost_el?.nativeElement, {
-      mask: Number,
-      scale: 2,
-      radix: ".",
-      signed: false,
-      max: 100,
-      lazy: false
+    // IMask(this.replacement_cost_el?.nativeElement, {
+    //   mask: Number,
+    //   scale: 2,
+    //   radix: ".",
+    //   signed: false,
+    //   max: 100,
+    //   lazy: false
+    // });
+    // IMask(this.length_el?.nativeElement, {
+    //   mask: Number,
+    //   scale: 1,
+    //   signed: false,
+    //   max: 240
+    // });
+
+    this.sel_year = this.$(this.year_el?.nativeElement).select2({
+      "data": this.years.map(year => ({ "id": year, "text": year })),
+    }).on("select2:select", (event: any) => {
+      const { text } = event.params.data;
+      this.year?.setValue(+text);
     });
   }
 
   onSubmit() {
-    console.log(this.fg_film.value);
+    this.fg_film.markAllAsTouched();
+  }
+
+  onReset() {
+    this.sel_year.val(null).trigger("change");
+    this.sel_categories.val(null).trigger("change");
+    this.sel_language.val(null).trigger("change");
+
+    this.fg_film.reset();
   }
 }
