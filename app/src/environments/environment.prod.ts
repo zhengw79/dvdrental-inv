7c52617a9ee7a1035d4b3fdb804c81efd99ea339
Zheng Wang <cerebronzw@gmail.com>
2023-02-23 10:31:13 -0500

    main update remote portal url

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://36e6-2607-fea8-4a5c-5f59-4dbd-a8ba-e0ec-febf.ngrok.io/"
+  apiUrl: "https://c631-2607-fea8-4a5c-5f59-ddd7-67e4-2993-d86e.ngrok.io/"
 };
